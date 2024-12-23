12157d399d610ba3bb20927f474e3d6a8a1a9c41
Sadanya <2930048068@qq.com>
2024-12-23 20:37:31 +0800

    配置环境

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.VUE_APP_BASE_DIR,
   transpileDependencies: true,
   outputDir: 'docs'
 })
